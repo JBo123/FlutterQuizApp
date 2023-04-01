19290c1b87ac74baed617e84022aea381ec378d6
JBo123 <jakub.klucky05@gmail.com>
2023-04-01 17:28:35 +0200

    login as guest

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,4 +4,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
   final user = FirebaseAuth.instance.currentUser;
+  Future<void> annLogin() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } on FirebaseAuthException catch (e) {}
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 }
