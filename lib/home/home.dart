616309f6e9df7e45e16eceaf7ef4985166fe6987
JBo123 <jakub.klucky05@gmail.com>
2023-04-01 15:11:45 +0200

    reupload

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,24 +1,26 @@
+import 'package:firebase_flutter_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_flutter_app/login/login.dart';
+import 'package:firebase_flutter_app/topics/topics.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text(
-            'about',
-            style: Theme.of(context).textTheme.button,
-          ),
-          /*style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.amber[800]),
-          ),
-         */
-          onPressed: () => Navigator.pushNamed(context, '/about'),
-        ),
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('error'));
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
