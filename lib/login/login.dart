edccba5392676a667d09e6b949d0769fdebb204a
JBo123 <jakub.klucky05@gmail.com>
2023-04-04 22:03:39 +0200

    Revert "added google sign-in wich doesent work"
    This reverts commit f802385754b0e7a2571dd4ba461234974b83a1b6.

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -6,8 +6,64 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< HEAD
       appBar: AppBar(
         title: Text('login'),
+=======
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const FlutterLogo(
+              size: 150,
+            ),
+            Flexible(
+              child: LoginButton(
+                icon: FontAwesomeIcons.userNinja,
+                text: 'Continue as Guest',
+                loginMethod: AuthService().annLogin,
+                color: Colors.deepPurple,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+  const LoginButton(
+      {super.key,
+      required this.text,
+      required this.icon,
+      required this.color,
+      required this.loginMethod});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 20,
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(24),
+          backgroundColor: color,
+        ),
+        onPressed: () => loginMethod(),
+        label: Text(text, textAlign: TextAlign.center),
+>>>>>>> parent of f802385... added google sign-in wich doesent work
       ),
     );
   }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,4 +4,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
   final user = FirebaseAuth.instance.currentUser;
+<<<<<<< HEAD
+=======
+  Future<void> annLogin() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } on FirebaseAuthException catch (e) {}
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+>>>>>>> parent of f802385... added google sign-in wich doesent work
 }
